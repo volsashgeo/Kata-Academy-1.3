e87249273299a6c684fd32b99fce96f8135fe046
Aleksandr Volkov <volsashgeo@gmail.com>
2023-12-07 17:05:20 +0300

    one page left

--- index-js.js
+++ index-js.js
@@ -47,19 +47,36 @@ if (window.innerWidth >= 768) {
   swiper.destroy();
 }
 
-let brands = document.querySelector(".swiper-wrapper");
-let linkReadNext = document.querySelector(".link-read-next");
-let linkText = document.querySelector(".link-read-next__text");
-let linkPic = document.querySelector(".link-read-next__pic");
+let description = document.querySelector('.section-description')
+let linkReadNextMain = document.querySelector(".main .link-read-next");
+let linkTextMain = document.querySelector(".main .link-read-next__text");
+let linkPicMain = document.querySelector(".main .link-read-next__pic");
 
-linkReadNext.addEventListener("click", function () {
-  brands.classList.toggle('open-all-anchors');
-  if (linkText.innerHTML === "Показать все") {
-    linkText.innerHTML = "Скрыть";
-    linkPic.style.rotate = "180deg";
+let brands = document.querySelector(".swiper-wrapper");
+let linkReadNextBrands = document.querySelector(".brands .link-read-next");
+let linkTextBrands = document.querySelector(".brands .link-read-next__text");
+let linkPicBrands = document.querySelector(".brands .link-read-next__pic");
+
+
+linkReadNextMain.addEventListener("click", function () {
+  description.classList.toggle('section-description--open-all');
+  if (linkTextMain.innerHTML === "Читать далее") {
+    linkTextMain.innerHTML = "Скрыть";
+    linkPicMain.style.rotate = "180deg";
   } else {
-    linkText.innerHTML = "Показать все";
-    linkPic.style.rotate = "0deg";
+    linkTextMain.innerHTML = "Читать далее";
+    linkPicMain.style.rotate = "0deg";
+  }
+});
+
+linkReadNextBrands.addEventListener("click", function () {
+  brands.classList.toggle('brands--open-all');
+  if (linkTextBrands.innerHTML === "Показать все") {
+    linkTextBrands.innerHTML = "Скрыть";
+    linkPicBrands.style.rotate = "180deg";
+  } else {
+    linkTextBrands.innerHTML = "Показать все";
+    linkPicBrands.style.rotate = "0deg";
   }
 });
 
